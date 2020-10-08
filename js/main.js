93d06b09b0cffab26609e5c560854929c60c5914
kediloofficial <kidelo.in@gmail.com>
2020-10-08 23:19:06 +0530

    loading time reduced

--- js/main.js
+++ js/main.js
@@ -118,7 +118,7 @@
 	------------------------------------------------------ */
 
 	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
-	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+	var mailChimpURL = '#'
 
 
 	$('#mc-form').ajaxChimp({
